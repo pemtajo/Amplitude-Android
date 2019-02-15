a24f8a4d2c04f3e72b22147c2f8cf75705b9f33e
Danilo Malafaia <leite.dfm@gmail.com>
2019-02-15 18:03:40 -0200

    adapting to gradle

---
+++ magnitude/src/main/java/com/magnitude/api/NetworkClient.java
@@ -0,0 +1,12 @@
+package com.magnitude.api;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Response;
+
+import java.io.IOException;
+
+public interface NetworkClient {
+
+    Response uploadEvents(EventUploadRequest eventUploadRequest, OkHttpClient client) throws IOException, IllegalArgumentException;
+
+}
